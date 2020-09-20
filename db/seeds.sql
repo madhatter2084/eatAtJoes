69bea065d58dcb2ffdc844c9627b4c185f6cbd67
madhatter <matthew.rikard@gmail.com>
2020-09-20 16:29:31 -0400

    seeds.sql created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon BBQ Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', FALSE);
